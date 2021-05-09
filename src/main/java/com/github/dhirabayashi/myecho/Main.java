dfb03729d1de87c51246b1f5a6bbc6ef226e16f3
daiki <gigabowser@hotmail.co.jp>
2021-05-09 14:28:53 +0900

    テスト追加

--- src/main/java/com/github/dhirabayashi/myecho/Main.java
+++ src/main/java/com/github/dhirabayashi/myecho/Main.java
@@ -2,6 +2,10 @@ package com.github.dhirabayashi.myecho;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(String.join(" ", args));
+        System.out.println(join(args));
+    }
+
+    static String join(String... args) {
+        return String.join(" ", args);
     }
 }
---
+++ src/test/java/com/github/dhirabayashi/myecho/MainTest.java
@@ -0,0 +1,23 @@
+package com.github.dhirabayashi.myecho;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void noArgs() {
+        assertEquals("", Main.join());
+    }
+
+    @Test
+    void oneArgs() {
+        assertEquals("aaa", Main.join("aaa"));
+    }
+
+    @Test
+    void twoArgs() {
+        assertEquals("aaa bbb", Main.join("aaa", "bbb"));
+    }
+
+}
